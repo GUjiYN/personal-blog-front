ca4b7ef01b58af6bc6528c65b3bb484cb2a95426
GUjiYN <2946459904@qq.com>
2024-10-16 20:45:49 +0800

    feat:登录页面完成（静态）

--- src/views/AuthPage/Login.vue
+++ src/views/AuthPage/Login.vue
@@ -1,87 +1,86 @@
 <template>
-  <div class="md:flex md:h-screen items-center md:justify-end sm:justify-center md:content-around pb-20">
-    <a-card
-        class="shadow-xl md:w-[calc(60%-5rem)] lg:w-[450px] mx-5 md:mx-10 lg:mx-36 mt-20 md:mt-0"
-    >
-      <div class="container p-3">
-        <div class="row-auto">
-          <div class="row-span-1 mb-8 text-center font-bold text-2xl">
-             登录
-          </div>
-          <div class="row-span-1">
-            <a-form
-                :label-col="{ span: 4 }"
+  <div class="md:flex md:h-screen items-center pb-20">
+    <!-- 左边的图片部分 -->
+    <div class="hidden md:block md:w-1/2 h-full">
+      <img src="@/assets/img1.jpg" alt="登录页面图片" class="w-full h-screen object-cover" />
+    </div>
 
-                class="userLogin-form"
-                name="normal_login"
-            >
-              <a-form-item
-                  :rules="[{ required: true, message: '请输入用户名' }]"
-                  label="用户名"
-                  name="user"
+    <!-- 右边的登录表单部分 -->
+    <div class="md:w-1/2 flex items-center justify-center">
+      <a-card
+          class="shadow-xl md:w-[calc(60%-5rem)] lg:w-[450px] mx-5 md:mx-10 lg:mx-36 mt-20 md:mt-0"
+      >
+        <div class="container p-3">
+          <div class="row-auto">
+            <div class="row-span-1 mb-8 text-center font-semibold text-2xl text-gray-700">
+              终端笔谈 - 登录
+            </div>
+            <div class="row-span-1">
+              <a-form
+                  :label-col="{ span: 4 }"
+                  class="userLogin-form"
+                  name="normal_login"
               >
-                <a-input >
-                  <template #prefix>
-
-                  </template>
-                </a-input>
-              </a-form-item>
-              <a-form-item
-                  :rules="[{ required: true, message: '请输入密码' }]"
-                  label="密码"
-                  name="password"
-              >
-                <a-input-password >
-                  <template #prefix>
-
-                  </template>
-                </a-input-password>
-              </a-form-item>
-              <div class="flex justify-end mb-3">
-                <div>
-                  还没有账户?
-                  <a class="text-aspargus hover:text-spring" >
-                    注册
-                  </a>
+                <a-form-item>
+                  <a-input placeholder="Username">
+                    <template #prefix><UserOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
+                  </a-input>
+                </a-form-item>
+                <a-form-item>
+                  <a-input type="password" placeholder="Password">
+                    <template #prefix><LockOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
+                  </a-input>
+                </a-form-item>
+                <div class="text-center">
+                  <!-- 修改按钮宽度 -->
+                  <a-button :disabled="false" class="bg-sky-500 w-64" html-type="submit" type="primary">
+                    <div class="mx-10">登录</div>
+                  </a-button>
                 </div>
-                <div class="mx-1">|</div>
-                <div>
-                  <a class="text-aspargus hover:text-spring" >忘记密码？</a>
+
+                <!-- 添加带有中间文字的横线 -->
+                <div class="flex items-center my-4">
+                  <div class="flex-grow border-t border-gray-300"></div>
+                  <span class="px-2 text-gray-500">或者</span>
+                  <div class="flex-grow border-t border-gray-300"></div>
                 </div>
-              </div>
-              <div class="text-center">
-                <a-button :disabled="false" class="bg-teal-400" html-type="submit" type="primary"
-                         >
-                  <div class="mx-10">登录</div>
-                </a-button>
-              </div>
-            </a-form>
+                <div class="text-center">
+                  <!-- 修改按钮宽度 -->
+                  <a-button :disabled="false" class="bg-sky-500 w-64" html-type="submit" type="primary">
+                    <div class="mx-10">注册</div>
+                  </a-button>
+                </div>
+
+              </a-form>
+            </div>
           </div>
         </div>
-      </div>
-    </a-card>
+      </a-card>
+    </div>
   </div>
 </template>
 
-<!--
+
+
+
 <script setup>
 import {onMounted, reactive, ref} from 'vue';
 import {message} from "ant-design-vue";
-import {KeyOutlined, UserOutlined} from "@ant-design/icons-vue";
+import {KeyOutlined, UserOutlined, LockOutlined} from "@ant-design/icons-vue";
 import router from "@/router/index.js";
-import {userLoginVO} from "@/assets/js/VoModel.js";
+/*import {userLoginVO} from "@/assets/js/VoModel.js";
 import {userLoginApi} from "@/api/AuthApi.js";
 import {userCurrentDO} from "@/assets/js/DoModel.js";
-import {getUserCurrentApi} from "@/api/UserApi.js";
+import {getUserCurrentApi} from "@/api/UserApi.js";*/
 
-const loginForm = reactive(userLoginVO);
+/*const loginForm = reactive(userLoginVO);
 const getUserCurrent = ref(userCurrentDO)
 
-/**
+/!**
  * 用户进行登陆操作
  * @return {Promise<void>}
  * @constructor
- */
+ *!/
 async function consoleUserLogin() {
   if (loginForm.user === '' && loginForm.password === '') {
     message.warn("账户密码不能为空")
@@ -103,10 +102,10 @@ async function consoleUserLogin() {
   }
 }
 
-/**
+/!**
  * 检查用户是否登陆
  * @return {Promise<void>}
- */
+ *!/
 async function isUserLogin() {
   switch (getUserCurrent.value.output) {
     case "Success":
@@ -125,6 +124,5 @@ async function isUserLogin() {
 onMounted(async _ => {
   getUserCurrent.value = await getUserCurrentApi();
   await isUserLogin();
-})
+})*/
 </script>
--->
