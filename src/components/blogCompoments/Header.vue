6c85f6115985ffcd19d7eeaedf30558b84f0f6a9
GUjiYN <2946459904@qq.com>
2024-10-21 15:08:01 +0800

    feat:系统主页导航栏，背景

--- src/components/blogCompoments/Header.vue
+++ src/components/blogCompoments/Header.vue
@@ -1,46 +1,44 @@
 <template>
-   <div class="relative bg-cover h-screen bg-[url('@/assets/images/img2.jpg')]">
-     <div class="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-35"></div>
-     <div class="relative">
-       <nav class="border-gray-200 p-5 fixed top-0 left-0 right-0 z-50 flex justify-center">
-         <div class="grid grid-cols-12 w-full justify-center items-center">
-           <div class="col-span-8  space-x-2">
-             <a class="flex items-center space-x-1 text-gray-200 font-semibold text-xl hover:text-gray-200" href="/">
-               <img
-                   alt="LOGO"
-                   class="w-10 h-10 rounded-lg"
-                   src="../../assets/images/favicon.ico"
-               />
-               <span>终端笔谈</span>
-             </a>
-           </div>
-           <div class="col-span-4 flex space-x-6 justify-end text-md">
-             <button @click="toggleSearch" class="flex items-center space-x-1 text-gray-200 hover:text-gray-200">
-               <HomeOutlined/>
-               <span>首页</span>
-             </button>
-             <button @click="toggleSearch" class="flex items-center space-x-1 text-gray-200 hover:text-gray-200">
-               <SearchOutlined />
-               <span>搜索</span>
-             </button>
-             <button @click="toggleSearch" class="flex items-center space-x-1 text-gray-200 hover:text-gray-200">
-               <LinkOutlined />
-               友链
-             </button>
-             <button @click="toggleSearch" class="flex items-center space-x-1 text-gray-200 hover:text-gray-200">
-               <MenuOutlined />
-               关于我
-             </button>
-           </div>
-         </div>
-       </nav>
-     </div>
-      <div class="relative flex items-center justify-center h-full">
-        <div class="text-center text-gray-200">
-          <h1 class="text-4xl font-bold mb-6">凌中的锋雨</h1>
+  <div class="relative bg-cover h-screen bg-[url('@/assets/images/img2.jpg')]">
+    <div class="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-35"></div>
+    <div class="relative">
+      <nav :class="['border-gray-200 py-3 px-5 fixed top-0 left-0 right-0 z-50 flex justify-center transition-all ' +
+            'duration-300', isScrolled ? 'bg-black/70' : 'bg-transparent']"
+      >
+        <div class="grid grid-cols-12 w-full justify-center items-center">
+          <div class="col-span-8 space-x-2">
+            <a class="flex items-center gap-2" href="/">
+              <img alt="LOGO" class="w-10 h-10 rounded-lg" src="@/assets/images/favicon.ico" />
+              <span class="text-gray-200 font-semibold text-xl hover:text-white">终端笔谈</span>
+            </a>
+          </div>
+          <div class="col-span-4 flex space-x-6 justify-end text-md">
+            <button @click="toggleSearch" class="flex gap-1 items-center space-x-1 text-gray-200 hover:text-white">
+              <HomeOutlined />
+              <span>首页</span>
+            </button>
+            <button @click="toggleSearch" class="flex gap-1 items-center space-x-1 text-gray-200 hover:text-white">
+              <SearchOutlined />
+              <span>搜索</span>
+            </button>
+            <button @click="toggleSearch" class="flex gap-1 items-center space-x-1 text-gray-200 hover:text-white">
+              <LinkOutlined />
+              <span>友链</span>
+            </button>
+            <button @click="toggleSearch" class="flex gap-1 items-center space-x-1 text-gray-200 hover:text-white">
+              <MenuOutlined />
+              <span>关于我</span>
+            </button>
+          </div>
         </div>
+      </nav>
+    </div>
+    <div class="relative flex items-center justify-center h-full">
+      <div class="text-center text-gray-200">
+        <h1 class="text-4xl font-bold mb-6">123</h1>
       </div>
     </div>
+  </div>
   <div v-show="isSearchVisible" class="fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center">
     <div class="bg-white rounded-lg shadow-lg w-full max-w-md p-10 relative">
       <form>
@@ -66,14 +64,30 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import {ref, onMounted, onUnmounted} from 'vue';
 import {SearchOutlined, LinkOutlined, MenuOutlined, HomeOutlined} from "@ant-design/icons-vue";
 
 // 搜索框显示状态
 const isSearchVisible = ref(false);
+const isScrolled = ref(false);
 
 // 切换搜索框显示状态
 const toggleSearch = () => {
   isSearchVisible.value = !isSearchVisible.value;
 };
+
+// 监听页面滚动事件，切换导航栏背景颜色
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 0;
+};
+
+// 当组件挂载时，监听滚动事件
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+// 当组件卸载时，移除滚动事件监听器
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
 </script>
