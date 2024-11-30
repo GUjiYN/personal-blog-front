ab90538cb54a840581f97d9c5ef1ac198a212a89
GUjiYN <2946459904@qq.com>
2024-11-30 18:36:22 +0800

    feat:完善友链页面导航栏

--- src/components/blogComponents/Header.vue
+++ src/components/blogComponents/Header.vue
@@ -215,7 +215,7 @@ const typeEffect = () => {
               <span>友链</span>
             </button>
             <a-dropdown>
-              <button @click="toggleSearch" :class="[isScrolled ? 'text-gray-700 hover:text-gray-700' : 'text-gray-200 hover:text-white']" class="flex gap-1 items-center space-x-1">
+              <button @click="toggleSearch" :class="[isScrolled ? 'text-gray-700 hover:text-gray-700' : 'text-white hover:text-white']" class="flex gap-1 items-center space-x-1">
                 <svg class="w-5 h-5" :class="isScrolled ? 'text-gray-700' : 'text-white'" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 5h6m-6 4h6M10 3v4h4V3h-4Z"/>
                 </svg>
@@ -230,7 +230,7 @@ const typeEffect = () => {
                 </a-menu>
               </template>
             </a-dropdown>
-            <button @click="toggleSearch" :class="[isScrolled ? 'text-gray-700 hover:text-gray-700' : 'text-gray-200 hover:text-white']" class="flex gap-1 items-center space-x-1">
+            <button @click="toggleSearch" :class="[isScrolled ? 'text-gray-700 hover:text-gray-700' : 'text-white hover:text-white']" class="flex gap-1 items-center space-x-1">
               <MenuOutlined />
               <span>关于我</span>
             </button>
